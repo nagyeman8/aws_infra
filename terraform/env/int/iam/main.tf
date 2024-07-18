841c3d60f0f14938855fd63a50c93d7a7f5cf612
nagyeman8 <61338713+nagyeman8@users.noreply.github.com>
2024-07-18 11:31:00 -0500

    adding env-int files (#7)
    Co-authored-by: nagyeman8 >

---
+++ terraform/env/int/iam/main.tf
@@ -0,0 +1,45 @@
+locals {
+  project_name = ""
+  env_name     = "int"
+  region       = ""
+}
+
+
+module "iam" {
+  source       = "../../../modules/iam"
+  env_name     = local.env_name
+  region       = local.region
+  project_name = local.project_name
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "bucket_name"
+    key    = "bucket_key"
+    region = ""
+  }
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=5.46.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = local.region
+}
